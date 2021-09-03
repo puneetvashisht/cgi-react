c25fc38de3476a248f747db9a42de1dbbfb6333b
Puneet Vashisht <puneetvashisht@gmail.com>
2021-09-03 16:27:05 +0530

    tet

--- my-redux-app/src/actions/auth-actions.js
+++ my-redux-app/src/actions/auth-actions.js
@@ -4,7 +4,22 @@ export const USER_LOGIN = 'USER_LOGIN';
 export const login = () => {
     console.log('Login action invoked');
     // simulation of authentication
-    return {type: USER_LOGIN, payload: true}
+
+    return dispatch => {
+  // Async code 
+        // setTimeout(()=>{
+        //     dispatch({type: USER_LOGIN, payload: true})
+        // }, 5000)
+        fetch('http://localhost:8080/auth')
+        .then(res=>res.json())
+        .then(data=> {
+            dispatch({type: USER_LOGIN, payload: data.success})
+        })
+
+    }
+  
+
+    
 }
 
 
